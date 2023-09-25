68227fa425c7136c52f5b2695bb91cd130256e5d
214PB <b0petbog@vasvari.org>
2023-09-25 09:08:32 +0200

    Warior osztály létrehozása, korábbi program javítása

--- WarriorProject/Program.cs
+++ WarriorProject/Program.cs
@@ -3,6 +3,7 @@
  */
 
 using System;
+using System.Net;
 
 namespace WarriorProject
 {
@@ -15,7 +16,7 @@ namespace WarriorProject
 
 
             public int X { get => x; set=> x = value;  }
-            public int Y { get => y; set => x = value; }
+            public int Y { get => y; set => y = value; }
 
          
             public Position(int x, int y)
@@ -23,14 +24,50 @@ namespace WarriorProject
                 this.x = x;
                 this.y = y;
             }
+
+            public void MoveHorizontal(int distance)
+            {
+                this.x = this.x + distance;
+            }
+            public void MoveVertical(int distance)
+            {
+                this.y = this.y + distance;
+            }
             public void ToConsole()
             {
                 Console.WriteLine($"{this.x}, {this.y}");
             }
+        }
+        class Warrior
+        {
+            private string name;
+            private Position p;
 
+            public Warrior(string name, Position p)
+            {
+                this.name = name;
+                this.p = p;
+            }
+
+            public void ToConsole()
+            {
+                Console.WriteLine($"{this.name}, position ({this.p.X}, {this.p.Y})");
+            }
+
+            public void MoveHorizontal(int distance)
+            {
+                p.MoveHorizontal(distance);
+            }
+
+            public void MoveVertical(int distance)
+            {
+                p.MoveVertical(distance);
+            }
         }
         static void Main(string[] args)
         {
+            
+
         }
     }
 }
