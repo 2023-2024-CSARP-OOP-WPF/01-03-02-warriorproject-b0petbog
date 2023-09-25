6a2c1e07f07d65371416b61e12bc99f27903ae7d
214PB <b0petbog@vasvari.org>
2023-09-25 09:08:57 +0200

    tesztelés

--- WarriorProject/Program.cs
+++ WarriorProject/Program.cs
@@ -66,7 +66,13 @@ namespace WarriorProject
         }
         static void Main(string[] args)
         {
-            
+            Position position = new Position(2, -1);
+            position.ToConsole();
+            Warrior warrior = new Warrior("Peter the Iron Man", position);
+            warrior.ToConsole();
+            warrior.MoveHorizontal(3);
+            warrior.MoveVertical(-2);
+            warrior.ToConsole();
 
         }
     }
