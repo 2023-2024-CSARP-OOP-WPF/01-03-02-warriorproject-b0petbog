7e3d042093e4b0846b2d55813e274bf49f2b610a
214PB <b0petbog@vasvari.org>
2023-09-25 08:15:32 +0200

    Position osztály létrehozása

--- WarriorProject/Program.cs
+++ WarriorProject/Program.cs
@@ -8,9 +8,29 @@ namespace WarriorProject
 {
     class Program
     {
+        class Position
+        {
+            private int x;
+            private int y;
+
+
+            public int X { get => x; set=> x = value;  }
+            public int Y { get => y; set => x = value; }
+
+         
+            public Position(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+            public void ToConsole()
+            {
+                Console.WriteLine($"{this.x}, {this.y}");
+            }
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
